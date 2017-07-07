488e53149cda7356312649ea3cc8ffb7512513a2
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2017-07-07 23:46:04 +0300

    Release script for image with real device support

---
+++ release_real.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    read -p "Task (build|push|all) : " TASK
+else
+    TASK=$1
+fi
+
+if [ -z "$2" ]; then
+    read -p "Release version: " RELEASE
+else
+    RELEASE=$2
+fi
+
+IMAGE="butomo1989/docker-android"
+FILE_NAME=docker/Real_device
+
+image_version="$IMAGE-real-device:$RELEASE"
+image_latest="$IMAGE-real-device:latest"
+
+function build() {
+  echo "[BUILD] Image name: $image_version and $image_latest"
+  echo "[BUILD] Dockerfile: $FILE_NAME"
+  docker build -t $image_version -f $FILE_NAME .
+  docker tag $image_version $image_latest
+}
+
+function push() {
+  echo "[PUSH] Image name: $image_version and $image_latest"
+  docker push $image_version
+  docker push $image_latest
+}
+
+case $TASK in
+    build)
+        build
+    ;;
+    push)
+        push
+    ;;
+    all)
+        build
+        push
+    ;;
+    *)
+        echo "Invalid environment! Valid options: test, build, push, all"
+    ;;
+esac
