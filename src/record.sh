fcffca21ff35d15bb7305ace6516c810c285788b
butomo1989 <budi.ut.1989@gmail.com>
2017-10-27 21:02:00 +0200

    Fixed error due to file name

--- src/record.sh
+++ src/record.sh
@@ -2,20 +2,18 @@
 
 function start() {
     mkdir -p $VIDEO_PATH
-    sw=$(($SCREEN_WIDTH - 1))
-    sh=$(($SCREEN_HEIGHT - 1))
-    name="$DEVICE-$BROWSER-$(date '+%d/%m/%Y-%H:%M:%S')"
+    name="$(date '+%d_%m_%Y_%H_%M_%S').mp4"
     echo "Start video recording"
-    ffmpeg -video_size $swx$sh -framerate 15 -f x11grab -i ${DISPLAY} $VIDEO_PATH/$name -y
+    ffmpeg -video_size 1599x899 -framerate 15 -f x11grab -i $DISPLAY $VIDEO_PATH/$name -y
 }
 
 function stop() {
     echo "Stop video recording"
-    kill $(ps -ef | grep ffmpeg)
+    kill $(ps -ef | grep ffmpeg | awk '{print $2}')
 }
 
 function auto_record() {
-    if [ ! -z $AUTO_RECORD ]; then
+    if [ $AUTO_RECORD ]; then
         if [ ${AUTO_RECORD,,} = true ]; then
             echo "Auto recording is enable. It will record the video automatically as soon as appium receive test scenario!"
 
@@ -23,7 +21,7 @@ function auto_record() {
             no_test=true
             while $no_test; do
                 task=$(curl -s localhost:4723/wd/hub/sessions | jq -r '.value')
-                if [ -n "$task" ]; then
+                if [ -n $task ]; then
                     sleep .5
                 else
                     no_test=false
@@ -34,7 +32,7 @@ function auto_record() {
             # Check if test is finished
             while [ $no_test = false ]; do
                 task=$(curl -s localhost:4723/wd/hub/sessions | jq -r '.value')
-                if [ -n "$task" ]; then
+                if [ -n $task ]; then
                     stop
                 else
                     sleep .5
@@ -43,3 +41,5 @@ function auto_record() {
         fi
     fi
 }
+
+$@
