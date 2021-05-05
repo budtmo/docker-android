fcb1f2ec31746e500923bd558b7648ed17cec663
Scott Wallace <scott@wallace.sh>
2021-05-05 10:04:46 +0100

    Fix an issue when attempting to detect an existing AVD image
    Fixes #286.

--- src/app.py
+++ src/app.py
@@ -56,7 +56,7 @@ def is_initialized(device_name) -> bool:
     if os.path.exists(config_path):
         logger.info('Found existing config file at {}.'.format(config_path))
         with open(config_path, 'r') as f:
-            if any('hw.device.name={}'.format(device_name) in line for line in f):
+            if any('hw.device.name = {}'.format(device_name) in line for line in f):
                 logger.info('Existing config file references {}. Assuming device was previously initialized.'.format(device_name))
                 return True
             else:
