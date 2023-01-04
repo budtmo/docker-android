ffe5f10ad76e3c84c7258782c6ecb624b5534ecb
Lerry <lerry.ortiz@gmail.com>
2023-01-03 21:14:48 -0800

    Remove logic for selenium 4 since has to be moved to appium/appium-docker-android repo

--- src/appium.sh
+++ src/appium.sh
@@ -300,29 +300,18 @@ _EOF
 function run_appium() {
 	echo "Preparing appium-server..."
 	CMD="appium --log $APPIUM_LOG"
-	CMD_CONNECT_TO_GRID_4=""
-	if [ "$CONNECT_TO_GRID" = true ] && ([ "$CONNECT_TO_GRID_4" = false ] || [ -z "$CONNECT_TO_GRID_4" ]); then
+	if [ "$CONNECT_TO_GRID" = true ]; then
 		NODE_CONFIG_JSON="/root/src/nodeconfig.json"
-		/root/src/generate_config.sh $NODE_CONFIG_JSON
+		/root/generate_config.sh $NODE_CONFIG_JSON
 		CMD+=" --nodeconfig $NODE_CONFIG_JSON"
-  	elif [ "$CONNECT_TO_GRID_4" = true ]; then
-    	DOWNLOAD_URL_SELENIUM_SERVER="https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.4.jar"
-    	NODE_CONFIG_TOML="/opt/bin/config.toml"
-    	START_SELENIUM_NODE="/opt/bin/start-selenium-grid-node-docker.sh"
-		/root/src/generate_config.sh $NODE_CONFIG_TOML
-		echo "Starting new node configuration for selenium grid 4"
-		CMD_CONNECT_TO_GRID_4+="& sleep 2s; wget -O /opt/selenium/selenium-server.jar $DOWNLOAD_URL_SELENIUM_SERVER && bash $START_SELENIUM_NODE"
-  	else
-    	echo "Skipping selenium grid connection"
   	fi
-
 
 	if [ "$RELAXED_SECURITY" = true ]; then
 		CMD+=" --relaxed-security"
 	fi
 
-	echo "Preparation is done"
-	TERM="xterm -T AppiumServer -n AppiumServer -e $CMD $CMD_CONNECT_TO_GRID_4"
+	echo "Preparation is done"  	
+	TERM="xterm -T AppiumServer -n AppiumServer -e $CMD"
 	$TERM
 }
 
@@ -383,5 +372,5 @@ elif [ "$GENYMOTION" = true ]; then
 	esac
 else
 	echo "Using Emulator"
-	bash /root/src/run_app.sh
+	python3 -m src.app
 fi
